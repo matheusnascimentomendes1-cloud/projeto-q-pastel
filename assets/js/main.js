71e7a9579534249960682a35ce8eb35f3f417d53
PiorAinda <matheusnascimentomendes1@gmail.com>
2026-08-20 10:47:22 -0300

    q-pastenelson

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const nome = prompt ("Digite seu nome");
+const sobrenome = prompt ("Digite seu sobrenome");
+
+const agora = new Date();
+
+const dia = agora.getDate();
+let mes = agora.getMonth()+ 1;
+const ano = agora.getFullYear();
+
+let hora = agora.getHours();
+const minuto = agora.getMinutes();
+
+if (dia < 10) dia = "0" + dia;
+if (mes < 10) mes = "0" + mes;
+if (hora < 10) hora = "0 " + hora;
+if (minuto < 10) minuto = "0" + minuto;
+
+console.log(nome + " " + sobrenome + "\nQuinta-Feira, " + dia + "/" + mes + "/" + ano + " - " + hora + ":" + minuto + "(-03:00)");
